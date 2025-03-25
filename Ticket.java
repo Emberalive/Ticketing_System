d719d41b585c4af5037ac44d9e90c02e5d44f49e
Sammy Boy <samueltsutcliffe@gmail.com>
2025-03-25 19:22:04 +0000

    added the getters and the setters for the Ticket class

--- Ticket.java
+++ Ticket.java
@@ -3,4 +3,22 @@ public class Ticket {
     private String issue;
     private String id;
 
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getIssue() {
+        return issue;
+    }
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+    public int getTicketID() {
+        return ticketID;
+    }
+    public void setTicketID(int ticketID) {
+        this.ticketID = ticketID;
+    }
 }
