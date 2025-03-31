925b1945210d354a4bbcb0c13513acca4f5ccade
Sammy Boy <samueltsutcliffe@gmail.com>
2025-03-31 22:59:30 +0100

    added some exttra security  to my error handling for the database class, as well as fixed the issues with connectivity

--- BDAccess/db_access.java
+++ BDAccess/db_access.java
@@ -4,14 +4,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class db_access {
-    private static final String URL = "jdbc:postgresql://192.168.0.16:5432/itticketing";
+    private static final String URL = "jdbc:postgresql://86.19.219.159:5432/itticketing";
     private static final String USER = "samuel";
     private static final String PASSWORD = "QwErTy1243!";
 
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            if (conn != null) {
+            if (conn != null && conn.isValid(2)) {
                 System.out.println("Connected to PostgreSQL successfully!");
+            } else {
+                System.out.println("Failed to connect to PostgreSQL!");
             }
         } catch (SQLException e) {
             System.out.println("Connection failed!" + e);
